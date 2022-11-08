1424b612fbd6ba1288cb743b4344b945f0ba17a1
Jhonatan <j.scordeiro@hotmail.com>
2022-11-08 12:32:02 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <component
+      v-for="(block, index) in page.blocks"
+      :key="index"
+      :is="components[block.name]"
+      v-bind="block"
+      :title="data.title"
+    >
+    </component>
+  </div>
+</template>
+<script setup>
+
+  const components = {
+    ComponentOne: resolveComponent('ComponentOne'),
+    ComponentTwo: resolveComponent('ComponentTwo')
+  }
+  
+  const page = {
+    blocks: [
+      {
+        name: "ComponentOne",
+        data: {
+          title: 'Componente 1'
+        }
+      },
+      {
+        name: "ComponentOne",
+        data: {
+          title: 'Componente 2'
+        }
+      }
+    ]
+  }
+
+</script>
---
+++ components/ComponentOne.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+    Component: {{ foo }}
+  </div>
+</template>
+<script lang="ts" setup>
+  import {ComponentObjectPropsOptions} from "vue";
+
+  interface Props {
+    foo: string
+    bar?: number
+  }
+
+  const props = defineProps<ComponentObjectPropsOptions<Props>>({
+    foo: {
+      type: String, 
+      required: true,
+      validator(value: unknown): boolean {
+        return true
+      }
+    },
+    bar: Number
+  })
+</script>
+
+<style scoped></style>
